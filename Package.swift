a79c35ba717e3f39e6b6769f79f9f0ff89624ffd
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2025-06-04 14:10:34 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "JamfKiller",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v13)
     ],
     products: [
         .executable(name: "JamfKiller", targets: ["JamfKiller"])
